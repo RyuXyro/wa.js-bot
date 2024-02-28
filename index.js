17aa325067ee4de1635c564028f281dcbf86e006
Risky <d4rknewbie2@gmail.com>
2024-02-28 18:30:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const qrcode = require('qrcode-terminal');
+// const { get } = require("request-promise-native");
+const fetch = require('node-fetch');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: 'owner' })
@@ -15,7 +17,8 @@ client.on('qr', (qr) => {
 });
 
 client.on('ready', () => {
-    console.log(`\n\nBot is online\nBot Number: 1`);
+    console.log(`\n\n${user.username} is online\nBot Number: 1`);
+    // store.save({ session: 'session-owner' });
 });
 
 client.on('auth_failure', message => {
@@ -27,24 +30,67 @@ client.on('message', async (message) => {
 
 
     if (message.body === (`${p}ping`) || (`${p}Ping`)) {
-        await message.reply('pong');
+        await message.reply(`
+        API PONG: ${Math.round(client.ws.ping)} ms
+        Latency: ${message.createdTimestamp - Date.now()}
+        `);
         console.log(message.body);
     }
-    if (message.body === (`${p}group`) || (`${p}Group`) || (`${p}groupinfo`) || (`${p}Groupinfo`)) {
+    if (message.body === (`${p}group`) || (`${p}Group`) || (`${p}groupinfo`) || (`${p}Groupinfo`) || (`${p}grup`) || (`${p}Grup`)) {
         let msg = await message.getChat();
         if (msg.isGroup) {
             message.reply(`
             *Group Details*
             Name: ${msg.name}
-            Description: ${chat.description}
-            Created At: ${chat.createdAt.toString()}
-            Created By: ${chat.owner.user}
-            Participant count: ${chat.participants.length}
+            Description: ${msg.description}
+            Created At: ${msg.createdAt.toString()}
+            Created By: ${msg.owner.user}
+            Participant count: ${msg.participants.length}
             `);
+            console.log(message.body)
         } else {
             message.reply('Command hanya bisa dipakai di grup!');
+            console.log(message.body)
         }
     }
+    if (message.body === (`${p}advice`) || (`${p}Advice`)) {
+        const data = await fetch('https://api.adviceslip.com/advice').then(res => res.json());
+        try {
+            message.reply(`
+            Advice
+
+            ${data.slip.advice}
+        `)
+        }
+        catch (err) {
+            message.reply(`Error get`)
+        }
+
+    }
+    // if (message.body === ('anime')) {
+    //     let option = {
+    //         url: `https://kitsu.io/api/edge/anime?filter[text]=${args.join(" ")}`,
+    //         method: `GET`,
+    //         headers: {
+    //             'Content-Type': "application/vnd.api+json",
+    //             'Accept': "application/vnd.api+json"
+    //         },
+    //         json: true
+    //     }
+    //     message.sendMessage('Waitt....').then(msg => {
+    //         get(option).then(body => {
+    //             try {
+    //                 message.reply(`
+    //                 Judul: ${body.data[0].attributes.titles.en}
+    //                 Synopsis: ${body.data[0].attributes.synopsis}
+    //                 `)
+    //             }
+    //             catch (err) {
+    //                 message.reply('Ga ketemu cuy')
+    //             }
+    //         })
+    //     })
+    // }
     if (message.body === ('test')) {
         await message.reply('Ak udh on cuy');
         console.log(message.body);
