bee429a2ee64915b8a9a34add27c3fe234c643c2
Risky <d4rknewbie2@gmail.com>
2024-02-29 21:57:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const qrcode = require('qrcode-terminal');
 // const { get } = require("request-promise-native");
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const client = new Client({
-    authStrategy: new LocalAuth({ clientId: 'owner' })
+    authStrategy: new LocalAuth({
+        clientId: 'owner',
+        dataPath: './login-owner'
+    })
 });
 const moderator = new Client({
-    authStrategy: new LocalAuth({ clientId: 'mod' })
+    authStrategy: new LocalAuth({
+        clientId: 'mod',
+        dataPath: './login-mod-1'
+    })
 })
 const { prefix } = require('./config.json')
 const { badword } = require('./data.json')
@@ -15,15 +21,46 @@ const { p } = prefix
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 });
-
 client.on('ready', () => {
-    console.log(`\n\n${user.username} is online\nBot Number: 1`);
+    console.log(`\n\nBot is online\nBot Number: 1`);
     // store.save({ session: 'session-owner' });
 });
+
+client.on('group_join', (notification) => {
+    console.log('join', notification);
+    notification.reply('Some User Joined.');
+});
+client.on('group_leave', (notification) => {
+    console.log('leave', notification);
+    notification.reply('Some User Left.');
+});
+client.on('group_update', (notification) => {
+    console.log('update', notification);
+});
+client.on('group_admin_changed', (notification) => {
+    if (notification.type === 'promote') {
+        console.log(`You were promoted by ${notification.author}`);
+    } else if (notification.type === 'demote')
+        console.log(`You were demoted by ${notification.author}`);
+});
+
 
 client.on('auth_failure', message => {
     console.error('AUTH ERROR', message)
 })
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
+
+
+let rejectCalls = true;
+
+client.on('call', async (call) => {
+    console.log('Call received, rejecting. GOTO Line 261 to disable', call);
+    if (rejectCalls) await call.reject();
+    await client.sendMessage(call.from, `[${call.fromMe ? 'Outgoing' : 'Incoming'}] Phone call from ${call.from}, type ${call.isGroup ? 'group' : ''} ${call.isVideo ? 'video' : 'audio'} call. ${rejectCalls ? 'This call was automatically rejected by the script.' : ''}`);
+});
 
 client.on('message', async (message) => {
     console.log('The Bot Get Message', message)
@@ -31,7 +68,7 @@ client.on('message', async (message) => {
 
     if (message.body === (`${p}ping`) || (`${p}Ping`)) {
         await message.reply(`
-        API PONG: ${Math.round(client.ws.ping)} ms
+        PONG!!!
         Latency: ${message.createdTimestamp - Date.now()}
         `);
         console.log(message.body);
@@ -53,44 +90,69 @@ client.on('message', async (message) => {
             console.log(message.body)
         }
     }
-    if (message.body === (`${p}advice`) || (`${p}Advice`)) {
-        const data = await fetch('https://api.adviceslip.com/advice').then(res => res.json());
-        try {
-            message.reply(`
-            Advice
-
-            ${data.slip.advice}
-        `)
-        }
-        catch (err) {
-            message.reply(`Error get`)
-        }
-
+    if (message.body === (`${p}cs`)) {
+        await client.setDisplayName(`${args.join(" ")}`)
+        message.reply('Selesai... Nama sudah diganti')
     }
-    // if (message.body === ('anime')) {
-    //     let option = {
-    //         url: `https://kitsu.io/api/edge/anime?filter[text]=${args.join(" ")}`,
-    //         method: `GET`,
-    //         headers: {
-    //             'Content-Type': "application/vnd.api+json",
-    //             'Accept': "application/vnd.api+json"
-    //         },
-    //         json: true
+    // if (message.body === (`${p}advice`) || (`${p}Advice`)) {
+    //     const data = await fetch('https://api.adviceslip.com/advice').then(res => res.json());
+    //     try {
+    //         message.reply(`
+    //         Advice
+
+    //         ${data.slip.advice}
+    //     `)
     //     }
-    //     message.sendMessage('Waitt....').then(msg => {
-    //         get(option).then(body => {
-    //             try {
-    //                 message.reply(`
-    //                 Judul: ${body.data[0].attributes.titles.en}
-    //                 Synopsis: ${body.data[0].attributes.synopsis}
-    //                 `)
-    //             }
-    //             catch (err) {
-    //                 message.reply('Ga ketemu cuy')
-    //             }
-    //         })
-    //     })
+    //     catch (err) {
+    //         message.reply(`Error get`)
+    //     }
+
     // }
+    if (message.body === (`${p}info`) || (`${p}bot`)) {
+        let info = client.info
+        client.sendMessage(message.from, `
+            *Connection info*
+            User name: ${info.pushname}
+            My number: ${info.wid.user}
+            Platform: ${info.platform}
+        `);
+    }
+    if (message.body === (`${p}mediainfo`) && message.hasMedia) {
+        const attachmentData = await message.downloadMedia();
+        message.reply(`
+            *Media info*
+            MimeType: ${attachmentData.mimetype}
+            Filename: ${attachmentData.filename}
+            Data (length): ${attachmentData.data.length}
+        `);
+    }
+    if (message.body === `${p}resmedia` && message.hasQuotedMsg) {
+        const quotedMsg = await message.getQuotedMessage();
+        if (quotedMsg.hasMedia) {
+            const attachmentData = await quotedMsg.downloadMedia();
+            client.reply(message.from, attachmentData, { caption: 'Here\'s your requested media.' });
+        }
+        if (quotedMsg.hasMedia && quotedMsg.type === 'audio') {
+            const audio = await quotedMsg.downloadMedia();
+            await client.reply(message.from, audio, { sendAudioAsVoice: true });
+        }
+    }
+    if (message.body === `${p}typing`) {
+        const chat = await message.getChat();
+        chat.sendStateTyping();
+    }
+    if (message.body === `${p}recording`) {
+        const chat = await message.getChat();
+        chat.sendStateRecording();
+    }
+    if (message.body === `${p}clearstate`) {
+        const chat = await message.getChat();
+        chat.clearState();
+    }
+    if (message.body === `${p}backup`) {
+        await store.save({ session: 'login-owner' });
+        message.reply('Done...')
+    }
     if (message.body === ('test')) {
         await message.reply('Ak udh on cuy');
         console.log(message.body);
